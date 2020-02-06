d74d509919e3d4c7ec06e64c5c276151c4bf34bb
James Helms <helms.james@gmail.com>
2020-02-05 22:30:54 -0600

    uploaded rough schematic, still working on the js file

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 // $( document ).ready(function() {
 //     console.log( "ready!" );
 // });
+var APIKey = "1ab0bfe8d343049b298bfab694993cdd";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
 
 $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=1ab0bfe8d343049b298bfab694993cdd&units=imperial",
+    url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  })
+
+$("#submit").click(function callAPI(){$.ajax({url: queryURL, method: "GET"}).then(function(response) {
+    console.log(queryURL);
     console.log(response);
-  });
+});
+
+    $("#submit").click(function(){
+      alert("The paragraph was clicked.");
+    });
+  // $.ajax({
+//     url: "http://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=1ab0bfe8d343049b298bfab694993cdd&units=imperial",
+//     method: "GET"
+//   }).then(function(response) {
+//     console.log(response);
+//   });
+
+  function displayWeather(cityName, windSpeed, humidity, temperature){
+
+    $(".city").html("city name: " + cityName);
+    $(".wind").html("wind speed: " + response.wind.speed);
+    $(".humidity").html("humidity: " + response.main.humidity);
+    $(".temp").html("temp: " + response.main.temp);
+    };
+
+    displayWeather(response.name);
 
 //   https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=1ab0bfe8d343049b298bfab694993cdd
 
