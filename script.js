28889d13265f997a2f97182666fe56884dc2e673
James Helms <helms.james@gmail.com>
2020-03-03 19:11:00 -0600

    got the weather to populate 5 days; need to have city searched be persistent

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
 // $( document ).ready(function() {
 //     console.log( "ready!" );
 // });
+
+//onsubmit
+
+
 var APIKey = "1ab0bfe8d343049b298bfab694993cdd";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=" + APIKey;
+var searchedCity;
+var queryURL;
+var tempConversion = "&units=imperial";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
+$("#submit").click(function(){
+  console.log("Clicked!");
+  citySearched = $("#city-input").val();
+   queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearched + "&appid=" + APIKey + tempConversion;
+  let cities = localStorage.getItem("cities");
+  if(!cities) {
+    cities = [];
+  }
+  else {
+    cities = JSON.parse(cities);
+  }
 
-$("#submit").click(function callAPI(){$.ajax({url: queryURL, method: "GET"}).then(function(response) {
-    console.log(queryURL);
-    console.log(response);
+  cities.push(citySearched);
+  localStorage.setItem("cities", JSON.stringify(cities));
 });
 
-    $("#submit").click(function(){
-      alert("The paragraph was clicked.");
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   })
+  var searchButton = document.querySelector("#submit");
+  var temp1 = document.querySelector("#temp-1");
+  var hum1 = document.querySelector("#hum-1");
+
+  var day1Array = [];
+
+  searchButton.addEventListener("click", function() {
+    $.ajax({url: queryURL, method: "GET"}).then(function(response) {
+      console.log(response.list[0]);
+      var i;
+      for (i = 0; i < response.list.length; i+=8) {
+        day1Array.push(Object.assign(response.list[i], {icon: `http://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png`}));
+      }  
+      console.log(day1Array);
+      
+      $("#five-day > .col-sm-2").each(function(index, element){
+          console.log(index, element);
+          $(element).find('.temp').text(day1Array[index].main.temp);
+          $(element).find('.icon').attr("src", day1Array[index].icon);
+          $(element).find('.humidity').text(day1Array[index].main.humidity);
+          $(element).find('.uv-index').text(day1Array[index].main.uvIndex);
+      });
     });
+  });
+
+
+
   // $.ajax({
 //     url: "http://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=1ab0bfe8d343049b298bfab694993cdd&units=imperial",
 //     method: "GET"
@@ -24,15 +64,7 @@ $("#submit").click(function callAPI(){$.ajax({url: queryURL, method: "GET"}).the
 //     console.log(response);
 //   });
 
-  function displayWeather(cityName, windSpeed, humidity, temperature){
 
-    $(".city").html("city name: " + cityName);
-    $(".wind").html("wind speed: " + response.wind.speed);
-    $(".humidity").html("humidity: " + response.main.humidity);
-    $(".temp").html("temp: " + response.main.temp);
-    };
-
-    displayWeather(response.name);
 
 //   https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&appid=1ab0bfe8d343049b298bfab694993cdd
 
